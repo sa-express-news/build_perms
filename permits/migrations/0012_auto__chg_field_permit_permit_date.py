00b7702d8c1bd6279c873f4904e9bfabe3362983
Joseph Kokenge <jkokenge@gmail.com>
2014-12-01 18:54:55 -0600

    changed permit_date back to just DateField to reflect deletion of the timestamp portion of that data

---
+++ permits/migrations/0012_auto__chg_field_permit_permit_date.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+
+        # Changing field 'Permit.permit_date'
+        db.alter_column(u'permits_permit', 'permit_date', self.gf('django.db.models.fields.DateField')(null=True))
+
+    def backwards(self, orm):
+
+        # Changing field 'Permit.permit_date'
+        db.alter_column(u'permits_permit', 'permit_date', self.gf('django.db.models.fields.DateTimeField')(null=True))
+
+    models = {
+        u'permits.permit': {
+            'Meta': {'object_name': 'Permit'},
+            'address': ('django.db.models.fields.TextField', [], {'null': 'True'}),
+            'calcvltn': ('django.db.models.fields.DecimalField', [], {'null': 'True', 'max_digits': '14', 'decimal_places': '2'}),
+            'coodttm': ('django.db.models.fields.DateField', [], {'null': 'True'}),
+            'declvltn': ('django.db.models.fields.DecimalField', [], {'null': 'True', 'max_digits': '14', 'decimal_places': '2'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'latitude': ('django.db.models.fields.DecimalField', [], {'null': 'True', 'max_digits': '10', 'decimal_places': '7', 'blank': 'True'}),
+            'longitude': ('django.db.models.fields.DecimalField', [], {'null': 'True', 'max_digits': '10', 'decimal_places': '7', 'blank': 'True'}),
+            'permit_date': ('django.db.models.fields.DateField', [], {'null': 'True'}),
+            'permit_nr': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '12'}),
+            'permit_type': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True'}),
+            'temp_coodttm': ('django.db.models.fields.DateField', [], {'null': 'True'}),
+            'worktype': ('django.db.models.fields.CharField', [], {'max_length': '7', 'null': 'True'})
+        }
+    }
+
+    complete_apps = ['permits']
--- permits/models.py
+++ permits/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Permit(models.Model):
     permit_nr = models.CharField(max_length=12, unique=True)
-    permit_date = models.DateTimeField(auto_now=False, auto_now_add=False, null=True)
+    permit_date = models.DateField(auto_now=False, auto_now_add=False, null=True)
     permit_type = models.CharField(max_length=255, null=True)
     worktype = models.CharField(max_length=7, null=True)
     address = models.TextField(null=True)
