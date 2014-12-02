9a19cc542dc264c322d873e4c487b41694775dbc
Joseph Kokenge <jkokenge@gmail.com>
2014-12-02 16:51:40 -0600

    added geocode_accuracy field to database

--- permits/models.py
+++ permits/models.py
@@ -2,13 +2,14 @@ from django.db import models
 
 class Permit(models.Model):
     permit_nr = models.CharField(max_length=12, unique=True)
-    permit_date = models.DateField(auto_now=False, auto_now_add=False, null=True)
+    permit_date = models.DateField(auto_now=False, auto_now_add=False, null=True, blank=True)
     permit_type = models.CharField(max_length=255, null=True)
     worktype = models.CharField(max_length=7, null=True)
     address = models.TextField(null=True)
     declvltn = models.DecimalField(max_digits=14, decimal_places=2, null=True)
     calcvltn = models.DecimalField(max_digits=14, decimal_places=2, null=True)
-    temp_coodttm = models.DateField(auto_now=False, auto_now_add=False, null=True)
-    coodttm = models.DateField(auto_now=False, auto_now_add=False, null=True)  
+    temp_coodttm = models.DateField(auto_now=False, auto_now_add=False, null=True, blank=True)
+    coodttm = models.DateField(auto_now=False, auto_now_add=False, null=True, blank=True)  
     latitude = models.DecimalField(max_digits=10, decimal_places=7, null=True, blank=True)
     longitude = models.DecimalField(max_digits=10, decimal_places=7, null=True, blank=True)
+    geocode_accuracy = models.CharField(max_length=255, null=True)
