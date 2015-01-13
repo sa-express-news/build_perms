ead45deffe95940f0fbcde66302974bfe92cff8d
Joseph Kokenge <jkokenge@gmail.com>
2015-01-13 17:57:42 -0600

    model updated for aplty field, plus migration

---
+++ permits/migrations/0015_auto__add_field_applicant_aplty.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding field 'Applicant.aplty'
+        db.add_column(u'permits_applicant', 'aplty',
+                      self.gf('django.db.models.fields.CharField')(max_length=3, null=True),
+                      keep_default=False)
+
+
+    def backwards(self, orm):
+        # Deleting field 'Applicant.aplty'
+        db.delete_column(u'permits_applicant', 'aplty')
+
+
+    models = {
+        u'permits.applicant': {
+            'Meta': {'object_name': 'Applicant'},
+            'aplty': ('django.db.models.fields.CharField', [], {'max_length': '3', 'null': 'True'}),
+            'apno': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['permits.Permit']", 'to_field': "'permit_nr'"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'})
+        },
+        u'permits.permit': {
+            'Meta': {'object_name': 'Permit'},
+            'address': ('django.db.models.fields.TextField', [], {'null': 'True'}),
+            'calcvltn': ('django.db.models.fields.DecimalField', [], {'null': 'True', 'max_digits': '14', 'decimal_places': '2'}),
+            'coodttm': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'}),
+            'declvltn': ('django.db.models.fields.DecimalField', [], {'null': 'True', 'max_digits': '14', 'decimal_places': '2'}),
+            'geocode_accuracy': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'latitude': ('django.db.models.fields.DecimalField', [], {'null': 'True', 'max_digits': '10', 'decimal_places': '7', 'blank': 'True'}),
+            'longitude': ('django.db.models.fields.DecimalField', [], {'null': 'True', 'max_digits': '10', 'decimal_places': '7', 'blank': 'True'}),
+            'permit_date': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'}),
+            'permit_nr': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '12'}),
+            'permit_type': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True'}),
+            'temp_coodttm': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'}),
+            'worktype': ('django.db.models.fields.CharField', [], {'max_length': '7', 'null': 'True'})
+        }
+    }
+
+    complete_apps = ['permits']
