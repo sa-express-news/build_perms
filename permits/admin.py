f3c6649d220c1c925a2d2e4bca210817fe012ab8
Joseph Kokenge <jkokenge@gmail.com>
2015-01-27 17:55:42 -0600

    added urls and admin code to just view the shapes and check things out

--- permits/admin.py
+++ permits/admin.py
@@ -1,3 +1,4 @@
-from django.contrib import admin
+from django.contrib.gis import admin 
+from models import BexarTractsPop10
 
-# Register your models here.
+admin.site.register(BexarTractsPop10, admin.GeoModelAdmin)
