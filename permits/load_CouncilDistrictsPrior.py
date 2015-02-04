b4b2b23dd48720edaea083d8ce61658cca7a52a6
Joseph Kokenge <jkokenge@gmail.com>
2015-02-04 15:05:55 -0600

    dropped and readded councildistrictsprior, then reloaded the shape file

--- permits/load_CouncilDistrictsPrior.py
+++ permits/load_CouncilDistrictsPrior.py
@@ -11,13 +11,13 @@ CouncilDistrictsPrior_mapping = {
     'sq_miles' : 'Sq_Miles',
     'shape_leng' : 'Shape_Leng',
     'shape_area' : 'Shape_Area',
-    'mpoly'     : 'MULTIPOLYGON',
+    'geom'     : 'MULTIPOLYGON',
 }
 
 shp = os.path.abspath(os.path.join(os.path.dirname(__file__), 
-    'data/shp_files/city_of_sa_council_districts_prior_11_27_12/sPreviousCouncilDistrictsWGS84/sPreviousCouncilDistrictsWGS84.shp'))
+    'data/shp_files/city_of_sa_council_districts_prior_11_27_12/prior_wgs84/prior_wgs84.shp'))
 
 def run(verbose=True):
-    lm = LayerMapping(CouncilDistrictsPrior,shp,CouncilDistrictsPrior_mapping,transform=False, encoding='iso-8859-1')
+    lm = LayerMapping(CouncilDistrictsPrior,shp,CouncilDistrictsPrior_mapping, source_srs=4326, transform=False, encoding='iso-8859-1')
 
     lm.save(strict=True, verbose=verbose)
