e8a7f42a051830336e664616b84eed65b646a575
Joseph Kokenge <jkokenge@gmail.com>
2015-02-11 12:46:06 -0600

    being work on view

--- permits/views.py
+++ permits/views.py
@@ -1,3 +1,20 @@
+from django.http import HttpResponse, Http404
+from django.template import RequestContext, loader 
+
 from django.shortcuts import render
 
-# Create your views here.
+from permits.models import ShapePermit, CouncilDistrictsPost, CouncilDistrictsPrior
+
+def index(request):
+    years_prior = range(2003,2013)
+
+    post_dists = CouncilDistrictsPost.objects.all()
+
+    for dist in post_dists:
+        len(ShapePermit.objects.filter(geom__within=dist.geom).filter(permit_dat__range=("2012-01-01","2012-12-31")))
+
+
+    template = loader.get_template('permits/index.html')
+    context = {'years': years_prior}
+
+    return render(request, 'permits/index.html', context) 
