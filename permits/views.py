7e80d8244ee34b784eae63c4e62ef9afb2cef46b
Joseph Kokenge <jkokenge@gmail.com>
2015-02-20 17:27:53 -0600

    finalized summation of build perm amounts per council district

--- permits/views.py
+++ permits/views.py
@@ -1,13 +1,13 @@
 from django.http import HttpResponse, Http404
 from django.template import RequestContext, loader 
-
 from django.shortcuts import render
 
 from permits.models import ShapePermit, CouncilDistrictsPost, CouncilDistrictsPrior
+from django.db.models import Sum 
 
 def get_permits_by_year_by_district_post():
     post_permits_by_year_by_district = {}
-    years_post = range(2012,2015)
+    years_post = range(2013,2015)
 
     post_dists = CouncilDistrictsPost.objects.all()
 
@@ -46,12 +46,47 @@ def get_permits_by_year_by_district_prior():
     return prior_permits_by_year_by_district
 
 
+def sum_amounts_by_year_by_district_prior():
+    prior_permits_by_year_by_district_sum = {}
+    years_prior = range(2003,2013)
+
+    prior_dists = CouncilDistrictsPrior.objects.all()
+
+    for dist in prior_dists:
+        prior_permits_by_year_by_district_sum[dist.name] = {}
+        for year in years_prior:
+            prior_permits_by_year_by_district_sum[dist.name][year] = {}
+            print(prior_permits_by_year_by_district_sum)
+
+            start_date = "%d-01-01" % year
+            end_date = "%d-12-31" % year
+
+            amt_sum = ShapePermit.objects.filter(geom__within=dist.geom).filter(permit_dat__range=(start_date,end_date)).aggregate(Sum('declvltn'))
+
+            prior_permits_by_year_by_district_sum[dist.name][year].update({ 'total_amount' : amt_sum['declvltn__sum'] })
+            amt_per_acre = (amt_sum['declvltn__sum']/dist.acres)
+
+            prior_permits_by_year_by_district_sum[dist.name][year].update({'amt_per_acre' : amt_per_acre})
+
+    return prior_permits_by_year_by_district_sum
+
 
 def index(request):
 
-    get_permits_by_year_by_district_prior()
+    sum_amounts_by_year_by_district_prior()
+
 
     template = loader.get_template('permits/index.html')
-    context = {'post_tots': get_permits_by_year_by_district_post(), 'prior_tots': get_permits_by_year_by_district_prior() }
+    context = { 
+                'post_tots': get_permits_by_year_by_district_post(), 
+                'prior_tots': get_permits_by_year_by_district_prior(),
+                'amts': sum_amounts_by_year_by_district_prior() 
+              }
 
-    return render(request, 'permits/index.html', context) 
+    return render(request, 'permits/index.html', context)
+
+
+
+
+
+ 
