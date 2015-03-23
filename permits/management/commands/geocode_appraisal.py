d58c1ad2377c08e7c266ae8c02c735e257aa9f09
Joseph Kokenge <jkokenge@gmail.com>
2015-03-23 17:27:09 -0500

    futzed with geocode script

--- permits/management/commands/geocode_appraisal.py
+++ permits/management/commands/geocode_appraisal.py
@@ -3,22 +3,47 @@ from permits.models import AppraisalData
 from googlegeocoder import GoogleGeocoder
 from permits.lib.batcher import batch_qs
 import time
+import re
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
         geocoder = GoogleGeocoder()
-        batch_qs("helos")
         appraisals_to_geocode = AppraisalData.objects.filter(latitude = None).order_by('id')
 
         for start, end, total, qs in batch_qs(appraisals_to_geocode):
             print "Now processing %s - %s of %s" % (start +1, end, total)
             
             for appraisal in qs:
-                st_num = appraisal.situs_num
-                st_pref = appraisal.situs_street_prefix
-                street = appraisal.situs_street
-                str_suf = appraisal.situs_street_sufix
-                zip = appraisal.situs_zip
-                addy = "%s %s %s %s" % (st_num, st_pref, street, str_suf)
-                print(addy)         
+                if appraisal.latitude == None and appraisal.longitude == None:
+
+                    st_num = appraisal.situs_num.strip()
+                    st_pref = appraisal.situs_street_prefix.strip()
+                    street = appraisal.situs_street.strip()
+                    str_suf = appraisal.situs_street_sufix.strip()
+                    st_zip = ''
+
+                    if appraisal.situs_zip != 'N/A':
+                        st_zip = appraisal.situs_zip.strip()
+
+                    city = appraisal.city
+                    state = appraisal.situs_state
+                    just_city = ''
+                    
+                    if re.search('(?<=CITY OF).*', city) is not None:
+                        just_city = re.search('(?<=CITY OF).*', city).group(0).strip()
+
+                    addy = "%s %s %s %s, %s, %s %s" % (st_num, st_pref, street, str_suf, just_city, state, st_zip)
+                    print(addy)
+
+                    try:
+                        search = geocoder.get(addy)
+                        time.sleep(1.5)
+                        appraisal.latitude = search[0].geometry.location.lat
+                        appraisal.longitude = search[0].geometry.location.lng 
+                        appraisal.geocode_accuracy = search[0].geometry.location_type
+                        print("Latitude: %s, Longitude: %s, Location type: %s") % (appraisal.latitude, appraisal.longitude, appraisal.geocode_accuracy)
+                        appraisal.save()
+                    except: 
+                        print(("Couldn't geocode or already geocoded."))
+
